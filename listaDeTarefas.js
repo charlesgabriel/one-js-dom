f383e53b7ea600772e4a95b18cf1981bded0c88c
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-21 19:40:47 -0300

    implementado recurso para remover tarefa da lista/implemented feature to remove task from the list

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -14,6 +14,7 @@
     tarefa.innerHTML = conteudo;
 
     tarefa.appendChild(BotaoConcluir());
+    tarefa.appendChild(BotaoDeleta());
     lista.appendChild(tarefa);
 
     input.value = " ";
@@ -37,5 +38,23 @@
     const botaoConclui = evento.target;
     const tarefaCompleta = botaoConclui.parentElement;
     tarefaCompleta.classList.toggle('done');
+  };
+
+  const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement('button');
+
+    botaoDeleta.innerText = 'Deletar';
+    botaoDeleta.addEventListener('click', deletarTarefa);
+
+    return botaoDeleta;
+  };
+
+  const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target;
+    const tarefaCompleta = botaoDeleta.parentElement;
+
+    tarefaCompleta.remove();
+
+    return botaoDeleta;
   };
 })();
